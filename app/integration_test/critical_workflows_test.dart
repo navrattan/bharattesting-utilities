b8a33073b44e62ba1fafda81063f7fde59e394ba
Anshu Soni <anshusoni@Anshus-MacBook-Pro.local>
2026-04-08 01:22:37 +0530

    feat: configure Vercel build and auto-deployment from main

--- app/integration_test/critical_workflows_test.dart
+++ app/integration_test/critical_workflows_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:patrol/patrol.dart';
 import 'package:bharattesting_utilities/main.dart' as app;
